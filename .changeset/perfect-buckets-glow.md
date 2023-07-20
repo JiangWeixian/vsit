420a5172a24dfdd1c667c438dea92b6e6d954d7e
JW <jiangweixian1994@gmail.com>
2023-07-20 22:21:27 +0800

    feat: sw (#12)

---
+++ .changeset/perfect-buckets-glow.md
@@ -0,0 +1,5 @@
+---
+"vsit": patch
+---
+
+service worker cache in browser side
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 // import { VitePluginDocument } from 'vite-plugin-document'
 import inspect from 'vite-plugin-inspect'
+import { VitePWA } from 'vite-plugin-pwa'
 import solid from 'vite-plugin-solid'
 import { PluginVit } from 'vsit'
 
@@ -11,6 +12,30 @@ export default defineConfig({
   plugins: [
     PluginVit(),
     solid(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/esm\.sh\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'esm-sh',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
     // Not working in solidjs
     // pages(),
     // svgrs(),
