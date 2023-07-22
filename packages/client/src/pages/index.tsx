9867b0a7497c757da9918d510d3aff69bcc6b06e
JW <jiangweixian1994@gmail.com>
2023-07-22 22:07:15 +0800

    :fire: dead code

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -21,33 +21,14 @@ const useWS = (props: UseWSProps) => {
   let socket
   const importMetaUrl = new URL(import.meta.url)
   // use server configuration, then fallback to inference
-  const serverHost = 'localhost:undefined/'
   const socketProtocol = null || (importMetaUrl.protocol === 'https:' ? 'wss' : 'ws')
   const hmrPort = null
   const socketHost = `${null || importMetaUrl.hostname}:${hmrPort || importMetaUrl.port}${'/'}`
-  const directSocketHost = 'localhost:undefined/'
+  console.log(hmrPort)
   try {
     let fallback
     // only use fallback when port is inferred to prevent confusion
-    if (!hmrPort) {
-      fallback = () => {
-        // fallback to connecting directly to the hmr server
-        // for servers which does not support proxying websocket
-        socket = setupWebSocket(socketProtocol, directSocketHost, () => {
-          const currentScriptHostURL = new URL(import.meta.url)
-          const currentScriptHost = currentScriptHostURL.host
-                      + currentScriptHostURL.pathname.replace(/@vite\/client$/, '')
-          console.error('[vite] failed to connect to websocket.\n'
-                      + 'your current setup:\n'
-                      + `  (browser) ${currentScriptHost} <--[HTTP]--> ${serverHost} (server)\n`
-                      + `  (browser) ${socketHost} <--[WebSocket (failing)]--> ${directSocketHost} (server)\n`
-                      + 'Check out your Vite / network configuration and https://vitejs.dev/config/server-options.html#server-hmr .')
-        })
-        socket.addEventListener('open', () => {
-          console.info('[vite] Direct websocket connection fallback. Check out https://vitejs.dev/config/server-options.html#server-hmr to remove the previous connection error.')
-        }, { once: true })
-      }
-    }
+    // eslint-disable-next-line unused-imports/no-unused-vars
     socket = setupWebSocket(socketProtocol, socketHost, fallback)
   } catch (error) {
     console.error(error)
@@ -56,7 +37,7 @@ const useWS = (props: UseWSProps) => {
     const socket = new WebSocket(`${protocol}://${hostAndPath}`, 'vite-hmr')
     let isOpened = false
     socket.addEventListener('open', () => {
-      console.log('custom socket opened')
+      console.log('[vit] websocket opened')
       isOpened = true
     }, { once: true })
     // Listen for messages
@@ -76,7 +57,7 @@ const useWS = (props: UseWSProps) => {
         onCloseWithoutOpen()
         return
       }
-      console.log('[vite] server connection lost. polling for restart...')
+      console.log('server connection lost. polling for restart...')
       // await waitForSuccessfulPing(protocol, hostAndPath);
       location.reload()
     })
