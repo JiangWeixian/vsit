67b8ac82e8840242ab3cb037807bbcd99f32cc6a
JW <jiangweixian1994@gmail.com>
2023-07-22 22:00:09 +0800

    :sparkles: service worker autoupdate

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,10 @@
 /* @refresh granular */
 import Home from './pages/index'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
 export default function App() {
   return (
     <Home />
--- packages/client/src/vite-env.d.ts
+++ packages/client/src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client-react" />
+/// <reference types="vite-plugin-pwa/client" />
 
 declare module '*.svg'
