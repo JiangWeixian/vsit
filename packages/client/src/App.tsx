b30b331fc846725d3c3274c37c77dd69c876dcc3
JW <jiangweixian1994@gmail.com>
2023-11-05 02:21:43 -0600

    :ambulance: disable pwa (#23)

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,9 @@
 /* @refresh granular */
 import Home from './pages/index'
 
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 
-registerSW({ immediate: true })
+// registerSW({ immediate: true })
 
 export default function App() {
   return (
