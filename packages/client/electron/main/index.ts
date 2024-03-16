e8f86ae23d82b29582d1b9069a49189872dd9aee
JW <jiangweixian1994@gmail.com>
2024-03-16 20:42:07 +0800

    :loud_sound: overrides all console

--- packages/client/electron/main/index.ts
+++ packages/client/electron/main/index.ts
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const port = 8080
 Log.initialize()
-console.log = Log.log
+Object.assign(console, Log.functions)
 
 // The built directory structure
 //
