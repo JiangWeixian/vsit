8fa9f02d3d0637d620ae09432533fe98ad0ec7dc
JW <jiangweixian1994@gmail.com>
2024-04-24 22:06:26 +0800

    feat: resizeable cols (#31)
    * :sparkles: resizeable
    
    * :recycle: use js resizeable
    
    * :building_construction: common code
    
    * :sparkles: close modal after command
    
    * :sparkles: split command
    
    * :pencil: release note
    
    * :fire: remove useless styles

--- packages/client/src/lib/constants.ts
+++ packages/client/src/lib/constants.ts
@@ -1,1 +1,7 @@
 export const VIRUTAL_WEB_ID = 'virtual:web.ts'
+
+export const InitialCode = `import { uniq } from "esm.sh:lodash-es@4.17.21"
+const a = uniq([1, 2, 3, 3])
+const b: number = 1
+console.log(a, b, uniq)
+`
